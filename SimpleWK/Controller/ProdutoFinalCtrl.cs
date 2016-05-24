d779825333cf797ee08892147c484eda1db63fc2
paulo <paulo@PAULO>
2016-05-24 19:16:04 -0300

    Commit maneiro

---
+++ SimpleWK/Controller/ProdutoFinalCtrl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller {
+    class ProdutoFinalCtrl {
+
+        public String getColumnName(String valor) {
+            String nomeColuna = "";
+
+            switch (valor)
+            {
+                case "Nome":
+                    nomeColuna = "nome";
+                    break;
+                case "Descrição":
+                    nomeColuna = "descricao";
+                    break;
+                case "Quantidade":
+                    nomeColuna = "quantidade";
+                    break;
+                case "Valor final":
+                    nomeColuna = "valor_final";
+                    break;
+                case "Valor Custo":
+                    nomeColuna = "valor_custo";
+                    break;
+            }
+
+            return nomeColuna;
+        }
+
+    }
+}
