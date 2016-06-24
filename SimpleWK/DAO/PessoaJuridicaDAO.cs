5ffa71acfd15abce5c89bd89272f328af47c3a70
kaleo <kaleopiasse@gmail.com>
2016-06-23 22:14:25 -0300

    Juridica DAO VIEW

---
+++ SimpleWK/DAO/PessoaJuridicaDAO.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using MySql.Data.MySqlClient;
+
+namespace DAO
+{
+    public class PessoaJuridicaDAO
+    {
+        public void Create(Juridica pessoa)
+        {
+            Database dbSWK = Database.GetInstance();
+
+            String qryPessoa = String.Format("INSERT INTO pessoa (nome, email, telefone_fixo, telefone_movel, id_endereco) " +
+                 "VALUES('" +
+                 pessoa.Nome + "', '" +
+                 pessoa.Email + "', '" +
+                 pessoa.TelefoneFixo + "', '" +
+                 pessoa.TelefoneMovel + "', " +
+                 pessoa.Endereco.Id + ");");
+
+            dbSWK.ExecuteSQL(qryPessoa);
+            int idPessoa = dbSWK.GetId();
+
+            String qryJuridica = "INSERT INTO juridica (fk_id_pessoa, cnpj, razao_social) VALUES(" +
+               idPessoa + ", '" +
+               pessoa.Cnpj + "', '" +
+               pessoa.RazaoSocial + "');";
+
+            dbSWK.ExecuteSQL(qryJuridica);
+        }
+
+        public void Update(Juridica pessoa)
+        {
+            Database dbSWK = Database.GetInstance();
+
+            String qryPes = "UPDATE pessoa SET nome = '" +
+                pessoa.Nome + "', email = '" +
+                pessoa.Email + "', telefone_fixo = '" +
+                pessoa.TelefoneFixo + "', telefone_movel = '" +
+                pessoa.TelefoneMovel + "' WHERE id_pessoa = " +
+                pessoa.Id + ";";
+            dbSWK.ExecuteSQL(qryPes);
+
+            String qryJur = "UPDATE juridica SET cnpj = '" +
+                pessoa.Cnpj + "', razao_social = '" +
+                pessoa.RazaoSocial + "' WHERE fk_id_pessoa = " +
+                pessoa.Id + ";";
+            dbSWK.ExecuteSQL(qryJur);
+        }
+
+        public void Delete(Juridica pessoa)
+        {
+            Database dbSWK = Database.GetInstance();
+
+            String qryJur = "DELETE FROM juridica WHERE fk_id_pessoa = " + pessoa.Id + ";";
+            dbSWK.ExecuteSQL(qryJur);
+
+            String qryPes = "DELETE FROM pessoa WHERE id_pessoa = " + pessoa.Id + ";";
+            dbSWK.ExecuteSQL(qryPes);
+        }
+    }
+}
--- SimpleWK/View/PessoaJuridicaView.cs
+++ SimpleWK/View/PessoaJuridicaView.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using Model;
+using DAO;
+
 
 namespace View
 {
@@ -17,30 +19,57 @@ namespace View
             InitializeComponent();
         }
         //chama a tela de cadastro de endereço
-        private void btnLocalizacao_Click(object sender, EventArgs e) {
+        private void btnLocalizacao_Click(object sender, EventArgs e)
+        {
             EnderecoView end = new EnderecoView(localizacao);
             end.ShowDialog();
             end.Dispose();
         }
-               
 
-        private void btnBusca_Click(object sender, EventArgs e) {
+
+        private void btnBusca_Click(object sender, EventArgs e)
+        {
 
         }
 
-        private void btnAdicionar_Click(object sender, EventArgs e) {
-            Juridica pessoaJuridica = new Juridica();
-            if(localizacao.Bairro != null)
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            try
             {
-                MessageBox.Show(localizacao.Bairro);
-                CreatePessoaJuridica(pessoaJuridica);
-            }else
+                Juridica pessoaJuridica = new Juridica();
+                if (localizacao.Bairro != null)
+                {
+                    pessoaJuridica.Endereco = localizacao;
+                    createPessoaJuridica(pessoaJuridica);
+
+                    LocalizacaoDAO locDao = new LocalizacaoDAO();
+                    locDao.Create(pessoaJuridica.Endereco);
+                    int idEnd = Database.GetInstance().GetId();
+                    pessoaJuridica.Endereco.Id = idEnd;
+
+                    PessoaJuridicaDAO pjDao = new PessoaJuridicaDAO();
+
+                    pjDao.Create(pessoaJuridica);
+                    LimparCampos();
+
+                }
+                else
+                {
+                    MessageBox.Show("Você precisa cadastrar um endereço antes!");
+                }
+            }
+            catch (Exception p)
             {
-                MessageBox.Show("Você precisa cadastrar a localização antes!");
+                MessageBox.Show(p.ToString());
             }
         }
-        
-        private void CreatePessoaJuridica(Juridica juridica) {
+        private void LimparCampos()
+        {
+            lbID.Text = txtNome.Text = txtRazaoSocial.Text = txtCnpj.Text = txtTelefoneFixo.Text = txtTelefoneMovel.Text = txtEmail.Text = "";
+        }
+
+        private void createPessoaJuridica(Juridica juridica)
+        {
             juridica.Nome = txtNome.Text;
             juridica.RazaoSocial = txtRazaoSocial.Text;
             juridica.Cnpj = txtCnpj.Text;
